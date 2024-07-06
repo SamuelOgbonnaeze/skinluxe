68a599ba6dd1a1f1a8050429c83eda70ee857656
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-07-06 03:19:14 +0100

    Footer done

--- components/footer.tsx
+++ components/footer.tsx
@@ -44,14 +44,14 @@ const Footer = () => {
                 </div>
                 {/* 2nd layer */}
                 <div className="bg-[#3D021E]  ">
-                    <div className=' z-10  bg-cover bg-no-repeat bg-center text-center items-center'>
+                    <div className=' '>
                         <div className="flex items-center justify-between w-full px-[108px] py-[80px] text-white">
 
                             {/* First half */}
                             <div className="flex w-[500px] space-x-10  justify-between">
                                 {/* How can we help */}
                                 <div className="flex flex-col space-y-2">
-                                    <h3 className="font-semibold  text-[24px] leading-[33.6px] mb-2 ">How Can We Help?</h3>
+                                    <h3 className="font-bold  text-[24px] leading-[33.6px] mb-2 ">How Can We Help?</h3>
                                     <div className="font-normal text-[16px] leading-[28px] ">
                                         <p>Skinluxe Branches</p>
                                         <p>Contact Us</p>
@@ -62,7 +62,7 @@ const Footer = () => {
                                 </div>
                                 {/* Products */}
                                 <div className="flex flex-col space-y-2">
-                                    <h3 className="font-semibold text-[24px] leading-[33.6px] mb-2 ">Products</h3>
+                                    <h3 className="font-bold text-[24px] leading-[33.6px] mb-2 ">Products</h3>
                                     <div className="gap-y-2 font-normal text-[16px] leading-[28px] ">
                                         <p>Men Skincare</p>
                                         <p>Women Skincare</p>
@@ -74,7 +74,7 @@ const Footer = () => {
                             {/* Second half */}
                             <div className="w-[600px]">
                                 <div className="">
-                                    <h3 className="font-semibold text-[24px] leading-[34px] ">Keep In Touch With Skinluxe</h3>
+                                    <h3 className="font-bold text-[24px] leading-[34px] ">Keep In Touch With Skinluxe</h3>
                                     <p className="font-normal text-[16px] leading-[28px] pt-[16px] ">Join The Skinluxe Newsletter And Be First To Hear About News, Offers And Skincare Advice</p>
                                 </div>
                                 <div className="mt-[16px]">
@@ -98,35 +98,72 @@ const Footer = () => {
                 <div className="flex bg-[#280114] items-center justify-between py-3 px-[108px] w-full text-[#79043C] ">
                     <div className="flex w-full items-center justify-between">
                         {/* left */}
-                        <div>
-                            k
+                        <div className="flex items-center justify-between my-3 gap-x-2 ">
+                            <div className="flex items-center gap-x-2 border-r-2 border-[#79043C]">
+                                <Image src="/location_on.png"
+                                    alt="location icon"
+                                    width={24}
+                                    height={24}
+                                />
+                                <p className="font-normal text-[16px] text-white leading-[28px] px-2 ">123 Wellington Bassey Way</p>
+                            </div>
+                            <div className="flex items-center gap-x-2 ">
+                                <Image src="/call.png"
+                                    alt="call icon"
+                                    width={24}
+                                    height={24}
+                                />
+                                <p className="font-normal text-[16px] text-white leading-[28px] px-2 ">234-904-500-4705</p>
+                            </div>
                         </div>
                         {/* right */}
-                        <div className="flex items-center justify-between gap-x-2 ">
+                        <div className="flex items-center w-[200px] justify-between gap-x-2 ">
                             <Image src="/instagram.png"
                                 alt="instagram icon"
                                 width={30}
                                 height={30}
-                                
+
                             />
                             <Image src="/facebook.png"
                                 alt="facebook"
                                 width={24}
                                 height={24}
-                              
+
                             />
                             <Image src="/twitter.png"
                                 alt="twitter icon"
                                 width={24}
                                 height={24}
-                                
+
                             />
                             <Image src="/tiktok.png"
                                 alt="tiktok icon"
                                 width={24}
                                 height={24}
-                              
+
                             />
+                        </div>
+                    </div>
+                </div>
+                {/* last layer */}
+                <div className="flex bg-[#14010A] items-center justify-between py-3 px-[108px] w-full text-[#CBCBCB] ">
+                    <div className="flex w-full items-center justify-between">
+                        {/* left */}
+                        <div className="flex items-center justify-between py-3 gap-x-2 ">
+                            <div className="flex items-center gap-x-2 ">
+                                <Image src="/copyright.png"
+                                    alt="copyright icon"
+                                    width={20}
+                                    height={20}
+                                />
+                                <p className="font-semibold text-[12px] leading-[16px] px-2 ">2024 Skinluxe. All Rights Reserved</p>
+                            </div>
+
+                        </div>
+                        {/* right */}
+                        <div className="flex items-center justify-between gap-x-6 ">
+                            <p className="font-normal text-[12px] leading-[22px]">Terms & Conditions</p>
+                            <p className="font-normal text-[12px] leading-[22px]">Privacy Policy</p>
                         </div>
                     </div>
                 </div>
