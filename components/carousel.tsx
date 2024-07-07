06018f228bd86f1f16de625ec6b352b8643ec160
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-07-07 15:03:52 +0100

    update

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Carousel = () => {
     return (
@@ -20,13 +21,15 @@ const Carousel = () => {
                 layout="fill"
                 className="absolute object-cover inset-0 z-10"
             />
-           <div className="relative w-full h-full flex flex-col items-center justify-center z-20 text-white text-center gap-y-6">
+            <div className="relative w-full h-full flex flex-col items-center justify-center z-20 text-white text-center gap-y-6">
                 <p className="text-[44px] font-openSans leading-[62px] font-bold">Unlock Your Natural Glow</p>
-                <div className="w-[184] border-2 py-2 px-4 border-white  ">
-                    <p className="line-through font-openSans font-normal text-[16px] leading-[28px]">Know More</p>
-                </div>
+                <Link href="/">
+                    <div className="w-[184] border-2 py-2 px-4 border-white  ">
+                        <p className="line-through font-openSans font-normal text-[16px] leading-[28px]">Know More</p>
+                    </div>
+                </Link>
             </div>
-            
+
         </div>
 
     );
