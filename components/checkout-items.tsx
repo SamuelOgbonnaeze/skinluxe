01289742ed8834489cdf46537cb171d8497cf341
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-07-08 20:04:10 +0100

    checkout page responsive

--- components/checkout-items.tsx
+++ components/checkout-items.tsx
@@ -9,13 +9,13 @@ import toast from "react-hot-toast";
 const CheckoutItem = () => {
 
     const handlePayment = () => {
-       toast.success("Payment Successful")
+        toast.success("Payment Successful")
     };
 
     return (
-        <div className="px-[75px] mt-[36px] ">
+        <div className="px-[16px] md:px-[75px] mt-[36px] ">
             {/* home to cart */}
-            <div className="w-[200px] flex items-center justify-between py-2">
+            <div className="w-[200px] hidden  md:flex items-center justify-between py-2">
                 <Link href="/">
                     <p className="text-[#606060] text-[16px] font-normal font-openSans leading-[28px] ">Home</p>
                 </Link>
@@ -29,19 +29,27 @@ const CheckoutItem = () => {
                 </Link>
             </div>
 
-            <div className=" mt-[26px] ">
-                <div className="flex flex-col  justify-start gap-y-3 px-[24px] ">
-                    <p className="font-openSans font-bold text-[31px] leading-[42px] ">Checkout</p>
-                    <p className="font-openSans font-normal text-[24px] leading-[34px] text-[#4B4C52] ">Select your preferred payment method</p>
+            <div className=" mt-[32px] lg:mt-[26px] ">
+                <div className="flex flex-col justify-start gap-y-3 px-[24px]">
+                    <p className="font-openSans font-semibold md:font-bold text-[18px] md:text-[31px] leading-[16px] md:leading-[42px]">
+                        Checkout
+                    </p>
+                    <p className="font-openSans font-normal text-[12px] md:text-[24px] leading-[13px] md:leading-[34px] text-[#4B4C52]">
+                        Select your preferred payment method
+                    </p>
                 </div>
             </div>
 
             <div className="px-[24px] py-[48px] ">
                 <div className="w-full grid grid-cols-3">
-                    <div className="col-span-2">
+                    <div className="col-span-3 lg:col-span-2">
                         <PaymentOptions />
+
+                        <div onClick={handlePayment} className="w-full mt-[31px] flex lg:hidden items-center justify-center bg-[#720439] text-white p-[10px] hover:cursor-pointer ">
+                            <p className="font-semibold text-[16px] leading-[22px] ">Pay now</p>
+                        </div>
                     </div>
-                    <div className="flex flex-col justify-start col-span-1 text-black font-openSans  pl-[50px] ">
+                    <div className="hidden lg:flex flex-col justify-start col-span-1 text-black font-openSans  pl-[50px] ">
                         <p className=" font-openSans font-bold text-[28px] leading-[39px] ">Order Summary</p>
                         <div className="mt-[24px] flex flex-col gap-y-[12px]">
                             <div className="w-full flex justify-between font-openSans text-[20px] leading-[28px]" >
--- components/payment-options.tsx
+++ components/payment-options.tsx
@@ -5,28 +5,27 @@ const PaymentOptions = () => {
     return (
 
 
-
-        <RadioGroup className="gap-y-[48px]" defaultValue="card">
-            <div className="flex  space-x-2">
+        <RadioGroup className="gap-y-[23px] md:gap-y-[48px]" defaultValue="card">
+            <div className="flex  space-x-2 border border-[#EDEDED] px-2 py-[17px] ">
                 <RadioGroupItem className="text-[#720439] " value="card" id="r1" />
-                <div className="flex flex-col justify-start gap-y-1 pl-[43px] ">
-                    <Label className="text-[20px] leading-[27px] font-openSans font-bold text-black " htmlFor="r1">Card</Label>
-                    <p className="font-normal font-openSans text-[16px] leading-[21px] text-[#4B4C52] " >Pay via your credit or debit card</p>
-                    <p className="text-[#720439] text-[13px] leading-[17px] font-openSans font-normal ">Recommended</p>
+                <div className="flex flex-col justify-start gap-y-1 md:pl-2 lg:pl-[43px] ">
+                    <Label className="text-[14px] md:text-[20px] leading-[19px] md:leading-[27px] font-openSans font-bold text-black " htmlFor="r1">Card</Label>
+                    <p className="font-normal font-openSans text-[10px] md:text-[16px] leading-[13px] md:leading-[21px] text-[#4B4C52] " >Pay via your credit or debit card</p>
+                    <p className="text-[#720439] text-[10px] md:text-[13px] leading:[13px] leading-[17px] font-openSans font-normal ">Recommended</p>
                 </div>
             </div>
-            <div className="flex items-center space-x-2">
+            <div className="flex space-x-2 border border-[#EDEDED] px-2 py-[17px]">
                 <RadioGroupItem className="text-[#720439] " value="transfer" id="r2" />
-                <div className="flex flex-col justify-start gap-y-1 pl-[43px] ">
-                    <Label className="text-[20px] leading-[27px] font-openSans font-bold text-black " htmlFor="r2">Bank transfer</Label>
-                    <p className="font-normal font-openSans text-[16px] leading-[21px] text-[#4B4C52] w-[500px] " >Transfer to the companies bank account. You will be required to confirm transaction.</p>
+                <div className="flex flex-col justify-start gap-y-1 md:pl-2 lg:pl-[43px] ">
+                    <Label className="text-[14px] md:text-[20px] leading-[19px] md:leading-[27px] font-openSans font-bold text-black " htmlFor="r2">Bank transfer</Label>
+                    <p className="font-normal font-openSans text-[10px] md:text-[16px] leading-[13px] md:leading-[21px] text-[#4B4C52] lg:w-[500px] " >Transfer to the companies bank account. You will be required to confirm transaction.</p>
                 </div>
             </div>
-            <div className="flex items-center space-x-2">
+            <div className="flex space-x-2 border border-[#EDEDED] px-2 py-[17px]">
                 <RadioGroupItem className="text-[#720439] " value="delivery" id="r3" />
-                <div className="flex flex-col justify-start gap-y-1 pl-[43px] ">
-                    <Label className="text-[20px] leading-[27px] font-openSans font-bold text-black " htmlFor="r3">Payment on delivery</Label>
-                    <p className="font-normal font-openSans text-[16px] leading-[21px] text-[#4B4C52] w-[500px] " >Pay our distributors with cash once they deliver to you.</p>
+                <div className="flex flex-col justify-start gap-y-1 md:pl-2 lg:pl-[43px] ">
+                    <Label className="text-[14px] md:text-[20px] leading-[19px] md:leading-[27px] font-openSans font-bold text-black " htmlFor="r3">Payment on delivery</Label>
+                    <p className="font-normal font-openSans text-[10px] md:text-[16px] leading-[13px] md:leading-[21px] text-[#4B4C52] lg:w-[500px] " >Pay our distributors with cash once they deliver to you.</p>
                 </div>
             </div>
         </RadioGroup>
