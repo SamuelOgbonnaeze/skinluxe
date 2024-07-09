d0184ce026bd6d767a4b0d0e99806e0ae8c835e6
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2024-07-09 03:34:14 +0100

    overview responsive

--- components/overview.tsx
+++ components/overview.tsx
@@ -3,46 +3,13 @@ import Image from "next/image";
 
 const Overview = () => {
     return (
-        <div className="px-[272px]">
+        <div className="px-[83px] lg:px-[272px]">
             <div className="  ">
-                <div className="flex flex-col items-center mx-auto text-center gap-y-2">
-                    <p className="text-[#720439] font-openSans font-semibold text-[24px] leading-[33px] ">Products</p>
-                    <p className="text-[#1E1E1E] font-openSans font-bold text-[44px] leading-[61px]  ">Glow In Abundance</p>
+                <div className="flex flex-col items-center  text-center gap-y-2">
+                    <p className="text-[#720439] font-openSans font-semibold text-[12px] md:text-[24px] leading-[16px] md:leading-[33px] ">Shop Our Products</p>
+                    <p className="text-[#1E1E1E] font-openSans font-bold text-[20px] md:text-[34px] lg:text-[44px] leading-[28px] md:leading-[40px] lg:leading-[61px]  ">Glow In Abundance</p>
                 </div>
-                <div className="mt-6 w-full ">
-                    <div className="flex items-center justify-between">
-                        <div className="">
-                            <Image
-                                src="/gift-1.png"
-                                alt=""
-                                width={256}
-                                height={342}
-                                className="fill"
-                            />
-                            <p className="mt-4 text-center text-[18px] leading-[32px] font-openSans font-normal ">Men Skin Care</p>
-                        </div>
-                        <div className="">
-                            <Image
-                                src="/gift-2.png"
-                                alt=""
-                                width={256}
-                                height={342}
-                                className="fill"
-                            />
-                            <p className="mt-4 text-center text-[18px] leading-[32px] font-openSans font-normal ">Women Skin Care</p>
-                        </div>
-                        <div className="">
-                            <Image
-                                src="/gift-3.png"
-                                alt=""
-                                width={256}
-                                height={342}
-                                className="fill"
-                            />
-                            <p className="mt-4 text-center text-[18px] leading-[32px] font-openSans font-normal ">Gifts & Sets</p>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
     
         </div>
